d15359362e86388474b05c0c1f5f9208d14db422
aradaki <takukanaspy@gmail.com>
2021-10-11 10:13:28 +0000

    [update]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/new'
+  get 'homes/edit'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
